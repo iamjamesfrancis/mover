1b9b422f9107ac21080cfae8303417a785b7c015
James Francis <james.francis@mhg-int.com>
2025-01-21 16:26:26 +0530

    Add .gitignore, main.py for mouse activity simulation, and requirements.txt

---
+++ main.py
@@ -0,0 +1,19 @@
+import time
+from datetime import datetime
+import pyautogui
+
+while True:
+
+    # Get the current mouse position
+    current_position = pyautogui.position()
+
+    # Move the mouse slightly to simulate activity
+    pyautogui.moveTo(current_position.x + 10, current_position.y + 10, duration=0.5)
+    pyautogui.moveTo(current_position, duration=0.5)  # Move back to the original position
+
+    current_time = datetime.now()
+    formatted_time = current_time.strftime("%H:%M:%S")  # Format: Hour:Minute:Second
+    print("Mouse moved!: ", formatted_time)
+    # Wait for 2 minutes (120 seconds)
+    # time.sleep(120)
+    time.sleep(60)
